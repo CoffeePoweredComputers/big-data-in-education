62eff54a4e315bced81b568fff0ee0cdae796f31
David H Smith IV <davidcrosbysmith7@gmail.com>
2020-04-19 23:18:29 -0700

    added assignment

---
+++ assignments/assign-3/assign-3.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+
+actions, observations = pd.read_csv('./data/actions.csv'), pd.read_csv('./data/observations.csv')
+
+print(actions)
+print(observations)
+
